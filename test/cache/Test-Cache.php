68b4430b2f873e39b1620a6fb191e73ce5416a70
Sinri Edogawa <e.joshua.s.e@gmail.com>
2018-09-07 15:03:14 +0800

    Remove core and io/curl and use libraries instead

--- test/cache/Test-Cache.php
+++ test/cache/Test-Cache.php
@@ -17,10 +17,3 @@ $cache->saveObject("key", "value", 3600);
 echo $cache->getObject("key") . PHP_EOL;
 $cache->removeObject("key");
 $cache->removeExpiredObjects();
-
-$cache = new \sinri\ark\cache\implement\ArkRedisCache("redis.sample.com", 6379, 255, "password");
-
-$cache->append("key", "value_appended_to_the_tail_of_old_value");
-$cache->increase("key", 1);
-$cache->decrease("key", 1);
-$cache->increaseFloat("key", 0.5);
